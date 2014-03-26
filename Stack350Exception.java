38393e7b416fafff6dd0efa626fc5f4ce37b6c05
itsagerston <itsagerston@aol.com>
2014-03-25 23:33:22 -0700

    Create Stack350Exception.java

---
+++ Stack350Exception.java
@@ -0,0 +1,13 @@
+/*
+ Any special exceptions you define MUST extend this class.
+ */
+
+class Stack350Exception extends Exception {
+    public Stack350Exception(){
+        super();
+    }
+	
+    public Stack350Exception(String msg) {
+        super(msg);
+    }
+}
